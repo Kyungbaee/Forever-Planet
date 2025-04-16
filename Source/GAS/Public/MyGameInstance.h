2ee97fcd5389142ca6bff1b0e6b29dfaeb29e8ed
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2025-04-16 14:38:22 +0900

    Initial Projects Files

---
+++ Source/GAS/Public/MyGameInstance.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+UCLASS()
+class GAS_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnStart() override;
+
+};
